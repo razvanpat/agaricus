27b79af9008eb772d789aaec0b50636c0e9a56c2
Razvan Pat <razvan.pat@zoho.com>
2013-01-17 22:09:15 +0100

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agaricus::Application.config.secret_token = '5362938730a5f42086138309df66f460c353fba0f0b9ba98a393f1e8f59f5acf806c6a90b6a42e91c61ab876f75ad76fd08051c13c2d610986f487143e2d86c7'
