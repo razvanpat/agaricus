58f8769828af479117b0016bc9cf291c29e9ff24
Razvan Pat <razvan.pat@zoho.com>
2013-01-22 21:22:38 +0200

    Implemented updating of budget when another with same month/year/category exists

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,9 @@
 class Budget < ActiveRecord::Base
   attr_accessible :category_id, :month, :value, :year
+  
+  before_save :remove_budgets_for_same_month
+
+  def remove_budgets_for_same_month
+    Budget.delete_all(["month = ? AND year = ? and category_id = ?", self.month, self.year, self.category_id])
+  end
 end
