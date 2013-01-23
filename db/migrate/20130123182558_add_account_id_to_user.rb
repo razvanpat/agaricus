94b6b89cb9d2956ff546002a750ecf328d0a9d1c
Razvan Pat <razvan.pat@zoho.com>
2013-01-23 22:54:38 +0100

    Changed style for menu to display in one line

--- db/migrate/20130123182558_add_account_id_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddAccountIdToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :account_id, :integer
-  end
-end
--- db/migrate/20130123183612_add_category_id_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryIdToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :category_id, :integer
-  end
-end
